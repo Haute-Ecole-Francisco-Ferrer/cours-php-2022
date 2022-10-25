9e1ada7b9c23bb24a97f258bf169e96d69104adc
Emmanuel Lemal <manuman73@gmail.com>
2022-10-25 14:24:15 +0200

    Update select.php

--- database/select.php
+++ database/select.php
@@ -1,7 +1,7 @@
-<!-- 
+
 <a href="select.php?genre=feminin">feminin</a>
 <a href="select.php?genre=masculin">masculin</a>
-<a href="select.php">Tous</a> -->
+<a href="select.php">Tous</a>
 
 
 <style>
@@ -25,10 +25,17 @@ require('connexion.inc.php');
 
 // 2 préparation de la requête
 // $maquery = $conn->prepare("SELECT * FROM etudiants ORDER BY nom ASC");
-$maquery = $conn->prepare("SELECT * FROM etudiants  ORDER BY nom ");
+$maquery = $conn->prepare("SELECT * FROM etudiants WHERE genre = :markergenre  ORDER BY nom ");
 
 // 3 exécution de la requête
-$maquery->execute();
+$maquery->execute([
+    'markergenre' => $_GET['genre']
+]);
+
+
+
+
+
 
 // 4 stocker les résultats dans un array
 $resultat = $maquery->fetchAll(PDO::FETCH_ASSOC);
@@ -46,9 +53,5 @@ foreach ($resultat as $row) { ?>
 <div class="<?php echo $row['genre']; ?>"><strong><?php echo $row['nom']; ?></strong> - <?php echo $row['prenom']; ?> </div>
 
 
- <?php }
-
-
-
-?>
+ <?php } ?>
 
