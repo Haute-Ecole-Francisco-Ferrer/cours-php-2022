042377d9f72c95a46ffe7e5b542739e47ea8b021
Emmanuel Lemal <manuman73@gmail.com>
2022-10-25 15:18:05 +0200

    Update select.php

--- database/select.php
+++ database/select.php
@@ -1,4 +1,6 @@
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 <a href="select.php?genre=feminin">feminin</a>
 <a href="select.php?genre=masculin">masculin</a>
 <a href="select.php">Tous</a><hr>
@@ -15,6 +17,7 @@
 .masculin {
     color: pink;
 }
+
 
     </style>
 
@@ -53,20 +56,32 @@ $maquery->execute();
 
 
 // 4 stocker les résultats dans un array
-$resultat = $maquery->fetchAll(PDO::FETCH_ASSOC);
+$resultat = $maquery->fetchAll(PDO::FETCH_ASSOC); 
 
 // echo '<pre>';
 // print_r($resultat);
-// echo '</pre>';
+// echo '</pre>'; 
 
+?>
+<div class="container">
+<table class="table table-striped">
+<tr><th>id</th><th>nom</th><th>prénom</th><th>genre</th><th>modifier</th><th>supprimer</th></tr>
 
+<?php
 //5 on boucle à travers les réultats
 
 foreach ($resultat as $row) { ?>
 
+<tr><td><?php echo $row['id']; ?></td><td><?php echo $row['nom']; ?></td><td><?php echo $row['prenom']; ?></td><td><?php echo $row['genre']; ?></td><td>modifier</td><td>supprimer</td></tr>
 
-<div class="<?php echo $row['genre']; ?>"><strong><?php echo $row['nom']; ?></strong> - <?php echo $row['prenom']; ?> </div>
 
 
  <?php } ?>
+
+</table>
+</div>
+
+
+
+
 
